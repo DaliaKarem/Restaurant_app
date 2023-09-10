bbc9ba394c098fca5a2e9f9d2cc6ce6ed1cb44aa
Dalia Karem <daliakarem0027@gmail.com>
2023-09-10 15:19:18 +0300

    Favorite

--- lib/modules/Favorite/presentation/Fav.dart
+++ lib/modules/Favorite/presentation/Fav.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/src/material/dialog.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+
 class Fav extends StatefulWidget {
   const Fav({Key? key}) : super(key: key);
 
@@ -7,8 +10,102 @@ class Fav extends StatefulWidget {
 }
 
 class _FavState extends State<Fav> {
+  final List<int> numbers = [1, 2, 3, 5, 8, 13, 21, 34, 55];
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return new Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Favorites",
+          style: TextStyle(color: Colors.black),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          onPressed: () => Navigator.of(context).pop(),
+          icon: Icon(
+            Icons.arrow_back,
+            color: Colors.black,
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.search,
+              color: Colors.black,
+            ),
+          ),
+        ],
+      ),
+      body: new ListView.builder(
+        padding: const EdgeInsets.all(16),
+        itemBuilder: (context, i) {
+          return Container(
+            decoration: BoxDecoration(borderRadius: BorderRadius.circular(12)),
+            height: 130,
+            child: Card(
+              elevation: 10,
+              child: Row(
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: Container(
+                      width: 100.0,
+                      height: 100.0,
+                      decoration: BoxDecoration(
+                          image: DecorationImage(
+                              image: NetworkImage(
+                                  "https://img.freepik.com/free-photo/double-hamburger-isolated-white-background-fresh-burger-fast-food-with-beef-cream-cheese_90220-1192.jpg"),
+                              fit: BoxFit.cover),
+                          borderRadius: BorderRadius.all(Radius.circular(75.0)),
+                          boxShadow: [
+                            BoxShadow(blurRadius: 7.0, color: Colors.black)
+                          ]),
+                    ),
+                  ),
+                  Column(
+                    children: [
+                      Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(
+                                top: 8.0, right: 50, bottom: 10),
+                            child: Text(
+                              "Burger",
+                              style: TextStyle(
+                                  fontSize: 15, fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          IconButton(onPressed: (){}, icon: Icon(Icons.favorite,color: Colors.red,))
+                        ],
+                      ),
+                      RatingBar.builder(
+                        itemSize: 20,
+                        initialRating: 3,
+                        minRating: 1,
+                        direction: Axis.horizontal,
+                        allowHalfRating: true,
+                        itemCount: 5,
+                        itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
+                        itemBuilder: (context, _) => Icon(
+                          Icons.star,
+                          color: Colors.amber,
+                        ),
+                        onRatingUpdate: (rating) {
+                          print(rating);
+                        },
+                      ),
+                      Text("Price : \$50")
+                    ],
+                  )
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
   }
 }
